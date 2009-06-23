5da37caa5d844a73325c643a1c425b28fd4dec5e
Naomi Luis <nluis@codeaurora.org>
2009-06-23 13:03:19 -0700

    development/samples: Fix for GlobalTime suspend/resume issue
    Change-Id: Ifa4fb4395e49beab2fe6da8a899313870fac3740
    CRs-Fixed: 177785

--- samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
+++ samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
@@ -1388,14 +1388,6 @@ class GTView extends SurfaceView implements SurfaceHolder.Callback {
         }
         mGLView.setNumTriangles(mNumTriangles);
         egl.eglSwapBuffers(mEGLDisplay, mEGLSurface);
-
-        if (egl.eglGetError() == EGL11.EGL_CONTEXT_LOST) {
-            // we lost the gpu, quit immediately
-            Context c = getContext();
-            if (c instanceof Activity) {
-                ((Activity)c).finish();
-            }
-        }
     }
 
 
