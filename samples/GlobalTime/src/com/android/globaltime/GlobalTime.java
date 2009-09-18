822ff453a790269db445820b06e5d3b3611eab14
Naomi Luis <nluis@codeaurora.org>
2009-09-18 15:35:28 -0700

    GlobalTime: Redraw textures for the flat world on resume
    The flat world object uses the textures drawn by the round world.
    Call the draw function on the round world object when resuming a suspended/timed out flat world
    screen in order to set the correct textures.
    
    Change-Id: I564bd8e38cb89cfbccb8d024b2f8c1fddab1d507
    CRs-fixed: 194001

--- samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
+++ samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
@@ -226,7 +226,8 @@ class GTView extends SurfaceView implements SurfaceHolder.Callback {
     private boolean mPaused = true;
     private boolean mHaveSurface = false;
     private boolean mStartAnimating = false;
-    
+    private boolean mResumeWorldFlat = false;
+
     public void surfaceCreated(SurfaceHolder holder) {
         mHaveSurface = true;
         startEGL();
@@ -344,6 +345,9 @@ class GTView extends SurfaceView implements SurfaceHolder.Callback {
     
     public void onResume() {
         mPaused = false;
+        if(mDisplayWorldFlat) {
+            mResumeWorldFlat = true;
+        }
         startEGL();
     }
     
@@ -1194,6 +1198,10 @@ class GTView extends SurfaceView implements SurfaceHolder.Callback {
         gl.glShadeModel(mSmoothShading ? GL10.GL_SMOOTH : GL10.GL_FLAT);
 
         gl.glTranslatef(mWrapX - 2, 0.0f, 0.0f);
+        if(mResumeWorldFlat) {
+            mWorld.draw(gl);
+            mResumeWorldFlat = false;
+        }
         worldFlat.draw(gl);
         gl.glTranslatef(2.0f, 0.0f, 0.0f);
         worldFlat.draw(gl);
