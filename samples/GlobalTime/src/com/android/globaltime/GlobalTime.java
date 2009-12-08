77c0efc17b1e8229ae490a8f46d55b11b349f038
Naomi Luis <nluis@quicinc.com>
2009-12-08 10:05:18 -0800

    GlobalTime: Perform a null check on the gtView object on resume
    Create a new gtView object, if the object is null when resuming the app.
    This fixes the null pointer exception encountered when resuming the app
    after receiving an MT call.

--- samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
+++ samples/GlobalTime/src/com/android/globaltime/GlobalTime.java
@@ -1469,6 +1469,11 @@ public class GlobalTime extends Activity {
 
     @Override protected void onResume() {
         super.onResume();
+        if(gtView == null)
+        {
+           gtView = new GTView(this);
+           setContentView(gtView);
+        }
         gtView.onResume();
         Looper.myQueue().addIdleHandler(new Idler());
     }
